80b418f3750752629b04678628efcc75232e5e09
mtclskn <umtclskn@gmail.com>
2020-06-03 03:17:15 +0300

    fix: rvix configuration was changed

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 export TURTLEBOT_3D_SENSOR=kinect
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/myworld.world " &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/worlds/myworld.world " &
+sleep 5
+xterm -e "rosrun rviz rviz -d /home/workspace/catkin_ws/src/rvizConfig/home_service.rviz" &
 sleep 5
 xterm -e "rosrun  turtlebot gmapping_demo.launch" &
-sleep 5
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
 xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch" &
