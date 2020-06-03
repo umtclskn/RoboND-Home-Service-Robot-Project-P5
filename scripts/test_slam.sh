a95213fc0aae33943c356d973cd0c654f840ba80
mtclskn <umtclskn@gmail.com>
2020-06-03 20:02:50 +0300

    fix: test slam updated

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -2,8 +2,8 @@
 export TURTLEBOT_3D_SENSOR=kinect
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/worlds/myworld.world " &
 sleep 5
-xterm -e "rosrun rviz rviz -d /home/workspace/catkin_ws/src/rvizConfig/home_service.rviz" &
+xterm -e "rosrun rviz rviz " &
 sleep 5
-xterm -e "rosrun  turtlebot gmapping_demo.launch" &
+xterm -e "rosrun gmapping slam_gmapping" &
 sleep 5
 xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch" &
