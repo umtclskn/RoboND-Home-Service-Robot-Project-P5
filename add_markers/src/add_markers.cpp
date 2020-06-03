42523f3742340b80f9852a05359d1ad06b19b0b1
mtclskn <umtclskn@gmail.com>
2020-06-03 03:16:44 +0300

    fix: map was changed

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -3,7 +3,7 @@
 #include "nav_msgs/Odometry.h"
 #include <complex>
 
-const float pickupZoneX = -4.22;
+const float pickupZoneX = 3.84;
 const float pickupZoneY = -6.88;
 const float pickupZoneZ = 1.0;
 
@@ -30,7 +30,7 @@ void odom_callback(const nav_msgs::Odometry::ConstPtr &msg)
         oclidDistance = sqrt(pow(currentPoseX - pickupZoneX, 2) + pow(currentPoseY - pickupZoneY, 2));
   }
 
-  if (oclidDistance < 0.4) {
+  if (oclidDistance < 0.3f) {
         // Pick up zone reached
 	if (!isPickupZone) {
 		isPickupZone = true;
@@ -54,24 +54,24 @@ static void updateMaker(){
 	marker.action = visualization_msgs::Marker::ADD;
 	marker.pose.position.x = dropOffZoneX;
 	marker.pose.position.y = dropOffZoneY;
-	marker.pose.position.z = 0;
 	marker.pose.orientation.x = 0.0;
 	marker.pose.orientation.y = 0.0;
 	marker.pose.orientation.z = 0.0;
 	marker.pose.orientation.w = 1.0;
 }
 
-
-
 static void createMarker(const uint8_t action)
 {
+	// Set the marker type.  can be a CUBE, SPHERE, ARROW, or CYLINDER
+	marker.type = visualization_msgs::Marker::CUBE;
+
 	// Set the frame ID and timestamp.  See the TF tutorials for information on these.
 	marker.header.frame_id = "map";
 	marker.header.stamp = ros::Time::now();
 
 	// Set the namespace and id for this marker.  This serves to create a unique ID
 	// Any marker sent with the same namespace and id will overwrite the old one
-	marker.ns = "basic_shapes";
+	marker.ns = "add_markers";
 	marker.id = 0;
 
 	marker.type = shape;
@@ -87,18 +87,18 @@ static void createMarker(const uint8_t action)
 	marker.pose.orientation.x = 0.0;
 	marker.pose.orientation.y = 0.0;
 	marker.pose.orientation.z = 0.0;
-	marker.pose.orientation.w = 0.0;
+	marker.pose.orientation.w = 1.0;
 
 	// Set the scale of the marker -- 1x1x1 here means 1m on a side
-	marker.scale.x = 0.4;
-	marker.scale.y = 0.4;
-	marker.scale.z = 0.4;
+	marker.scale.x = 0.2;
+	marker.scale.y = 0.2;
+	marker.scale.z = 0.2;
 
 	// Set the color -- be sure to set alpha to something non-zero!
-	marker.color.r = 1.0f;
-	marker.color.g = 0.0f;
+	marker.color.r = 0.0f;
+	marker.color.g = 1.0f;
 	marker.color.b = 0.0f;
-	marker.color.a = 1.0;
+	marker.color.a = 1.0f;
 
 	marker.lifetime = ros::Duration();
 }
@@ -106,27 +106,32 @@ static void createMarker(const uint8_t action)
 
 int main( int argc, char** argv )
 {
-	ros::init(argc, argv, "add_markers");
+ros::init(argc, argv, "add_markers");
 	ros::NodeHandle n;
 	ros::Rate r(1);
 	marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
 	odom_sub = n.subscribe("/odom", 10, &odom_callback);
 
 	createMarker(visualization_msgs::Marker::ADD);
-
-	// Publish the marker
-	while (marker_pub.getNumSubscribers() < 1)
-	{
-		if (!ros::ok())
-		{
+	
+        // Check for subscribers
+	while(marker_pub.getNumSubscribers()<1){
+		ROS_WARN("Please subscribe to the marker");
+		if(!ros::ok()){
 			return 0;
 		}
-		ROS_WARN_ONCE("Please create a subscriber to the marker");
-		sleep(1);
 	}
 
+	marker.action=visualization_msgs::Marker::ADD;
 	marker_pub.publish(marker);
-        ros::spinOnce();
+	ROS_INFO("Published marker at the pick_up point");
+	ros::Rate r(1);
+	bool picked=false;
+	while(ros::ok()){
+		ros::spinOnce();
+		r.sleep();
+	}
+	ROS_INFO("\n not shut down:%d",ros::ok());
+		
 }
-
 
