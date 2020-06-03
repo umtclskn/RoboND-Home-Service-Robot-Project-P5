6610912722063a83a800984c074774dc98296393
mtclskn <umtclskn@gmail.com>
2020-06-03 03:26:57 +0300

    fix: addmarkers.sh and pick_objects.sh scripts were updated

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/myworld.world " &
-sleep 10
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/map/map.yaml " &
-sleep 10
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
-sleep 10
-xterm -e " rosrun add_markers add_markers "
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch"&
+sleep 5
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch"&
+sleep 5
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch"&
+sleep 5
+xterm -e "rosrun add_markers add_markers"
--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-export TURTLEBOT_3D_SENSOR=kinect
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/myworld.world " &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch"&
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/map/map.yaml " &
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch"&
 sleep 5
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch"&
 sleep 5
-xterm -e "rosrun pick_objects pick_objects" &
+xterm -e "rosrun pick_objects pick_objects"
